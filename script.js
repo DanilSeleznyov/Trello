33484baf8aae9e94ded30a081f4275a15beeb80c
DanilSeleznyov <91893687+DanilSeleznyov@users.noreply.github.com>
2023-07-28 11:58:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const createBtn = document.querySelector('.create'),
-    tagInput = document.querySelector('.tags'),
-    settingsBtn = document.querySelector('.settings'),
-    addBtns = document.querySelectorAll('.add')
-    
-createBtn.addEventListener('click', showModalBoard)
-addBtns.forEach(el => {
-    el.addEventListener('click', showModalTask)
-});
